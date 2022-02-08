138d39deedef1aec2d9ea6e5bf392b8da3456f0d
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2022-02-08 10:28:01 +0000

    2 trailing decimals all time.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ bill = float(input("What is the total bill? $"))
 rate = float(input("What percent tip will you like to give? "))
 num = int(input("How many members you want to split? "))
 
-#Calculate the pay for each person
+#Calculate the pay for each person, round off to 2 decimal places
 each_pay = (bill / num) * (1+(rate/100))
+final_amt = round(each_pay, 2) 
 
-#Print amount rounded off with 2 decimals
-print("Each person should pay: $", round(each_pay,2))
+#Print amount with 2 decimal places on each case
+print("Each person should pay: ${:.2f}".format(each_pay))
